f5b3ee11120d25ffc44b907ea4fce694220b766e
carlosgonzalez96 <carlosgonz817@gmail.com>
2017-11-16 17:05:53 -0800

    working web app

--- RPS-script.js
+++ RPS-script.js
@@ -1,59 +1,65 @@
 function rock() {
-    var rock = 1;
     var computer = Math.floor((Math.random() * 3) + 1);
     document.getElementById("computer-output").innerHTML = computer;
     document.getElementById("player-pick").innerHTML = "ROCK";
     //computer output
     if(computer == 1) {
         document.getElementById("computer-output").innerHTML = "ROCK";
-        document.getElementById("win-lose").innerHTML = "TIE";
+        document.getElementById("output").innerHTML = "TIE";
+        document.getElementById("output").style.color = "skyblue";
     } else if(computer == 2) {
         document.getElementById("computer-output").innerHTML = "PAPER";
-        document.getElementById("win-lose").innerHTML = "YOU LOSE";
+        document.getElementById("output").innerHTML = "YOU LOSE";
+        document.getElementById("output").style.color = "Red";
     } else {
         document.getElementById("computer-output").innerHTML = "SCISSORS";
-        document.getElementById("win-lose").innerHTML = "YOU WIN";
+        document.getElementById("output").innerHTML = "YOU WIN";
+        document.getElementById("output").style.color = "green";
     }
 }
 
 function paper() {
-    var paper = 2;
     var computer = Math.floor((Math.random() * 3) + 1);
     document.getElementById("computer-output").innerHTML = computer;
     document.getElementById("player-pick").innerHTML = "PAPER";
     //computer output
     if(computer == 1) {
         document.getElementById("computer-output").innerHTML = "ROCK";
-        document.getElementById("win-lose").innerHTML = "YOU WIN";
+        document.getElementById("output").innerHTML = "YOU WIN";
+        document.getElementById("output").style.color = "green";
              
     } else if(computer == 2) {
         document.getElementById("computer-output").innerHTML = "PAPER";
-        document.getElementById("win-lose").innerHTML = "TIE";
+        document.getElementById("output").innerHTML = "TIE";
+        document.getElementById("output").style.color = "skyblue";
 
     } else {
         document.getElementById("computer-output").innerHTML = "SCISSORS";
-        document.getElementById("win-lose").innerHTML = "YOU LOSE";   
+        document.getElementById("output").innerHTML = "YOU LOSE";
+        document.getElementById("output").style.color = "Red";
 
     }
     
 }
 
 function scissors() {
-    var paper = 3;
     var computer = Math.floor((Math.random() * 3) + 1);
     document.getElementById("computer-output").innerHTML = computer;
     document.getElementById("player-pick").innerHTML = "SCISSORS";
     //computer output
     if(computer == 1) {
         document.getElementById("computer-output").innerHTML = "ROCK";
-        document.getElementById("win-lose").innerHTML = "YOU LOSE";
+        document.getElementById("output").innerHTML = "YOU LOSE";
+        document.getElementById("output").style.color = "Red";
  
     } else if(computer == 2) {
         document.getElementById("computer-output").innerHTML = "PAPER";
-        document.getElementById("win-lose").innerHTML = "YOU WIN";
+        document.getElementById("output").innerHTML = "YOU WIN";
+        document.getElementById("output").style.color = "green";
     } else {
         document.getElementById("computer-output").innerHTML = "SCISSORS";
-        document.getElementById("win-lose").innerHTML = "TIE";
+        document.getElementById("output").innerHTML = "TIE";
+        document.getElementById("output").style.color = "skyblue";
     }
 
 }
